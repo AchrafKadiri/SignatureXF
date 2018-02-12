2d89483f69118b24c63edc5d7aab41f81817334f
Achraf Kadiri Daidi <achraf.kadiri-daidi@sogeti.com>
2018-02-12 08:54:17 +0100

    Implemented signature pad functionallity.

---
+++ SignatureXF/SignatureXF/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using SignaturePad.Forms;
+using System;
+using System.IO;
+using SignaturePad.Forms;
+using Xamarin.Forms;
+
+namespace SignatureXF
+{
+    public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+
+        private async void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            Stream image = await PadView.GetImageStreamAsync(SignatureImageFormat.Jpeg);
+            // you can add code here to save the Stream image.
+        }
+
+        private void ClearButton_Clicked(object sender, EventArgs e)
+        {
+            PadView.Clear();
+        }
+    }
+}
